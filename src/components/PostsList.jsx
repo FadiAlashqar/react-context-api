8d42c5f23889bb338be2b826017959e922fc76e1
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-20 00:46:25 +0200

    [UPD] cards done

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import MainNav from './MainNav';
+import { useContext } from 'react';
+import PostContext from '../contexts/PostContext';
 
 const PostsList = () => {
-    const posts = [
-        { id: 1, title: 'React Basis', content: 'React apps are made out of components. A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page.', category: 'FrontEnd' },
-        { id: 2, title: 'What is Express.js?', content: 'Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.', category: 'Backend' },
-        { id: 3, title: 'Run JavaScript Everywhere', content: 'Node.js® is a free, open-source, cross-platform JavaScript runtime environment that lets developers create servers, web apps, command line tools and scripts.', category: 'Backend' },
-        { id: 4, title: 'Build fast, responsive sites with Bootstrap', content: 'Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins.', category: 'Frontend' },
-    ];
+
+    const products = useContext(PostContext);
 
 
     return (
@@ -19,20 +17,23 @@ const PostsList = () => {
                         <h1 className='mx-3'>POST LIST</h1>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-12">
-                        <ul>
-                            {posts.map((post) => {
-                                return <li key={post.id}>
-                                    <h3>{post.title}</h3>
-                                    <p>{post.content}</p>
-                                    <p><em>{post.category}</em></p>
-                                </li>
-                            })}
-                        </ul>
-                    </div>
+                <div className="row g-4 d-flex justify-content-center">
+                    {products.map((product) => {
+                        return <div className="col-12 col-md-6 col-lg-4">
+                            <div key={product.id} className="card p-3">
+                                <div className="card-title">
+                                    <h4>{product.title}</h4>
+                                </div>
+                                <div className="card-text">
+                                    <p>{product.content}</p>
+                                    <p><em>{product.category}</em></p>
+                                </div>
+                            </div>
+
+                        </div>
+                    })}
                 </div>
-            </div>
+            </div >
         </>
     )
 }
